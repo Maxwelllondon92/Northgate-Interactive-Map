802797bb865f75582eda8e0c8e7607e3a010b7ab
Maxwelllondon92 <Maxwelllondon92@gmail.com>
2020-10-27 21:32:30 +0100

    Update lang.js

--- assets/script/lang.js
+++ assets/script/lang.js
@@ -1,11 +1,11 @@
-var submit = document.getElementById('submit');
+var submit = document.getElementById('submit').value;
 submit.addEventListener('click', changeLang);
 
 function changeLang(){
-    var file = new XMLHttpRequest();
-    file.open("GET", 'assets/lang/en/example.txt', true);
-    file.onreadystatechange = function() {
-		  alert(file.responseText);
-        }
-		file.send();
-      }
+	var file;
+	fetch("../lang/en/example.json")
+		.then(res => res.json())
+		.then(data => file = JSON.parse(data));
+	alert(file.lang);
+	alert(file.text);
+}
