37b3db69f4819ca174d2ed06bc0f43179e27b688
Maxwelllondon92 <Maxwelllondon92@gmail.com>
2020-10-29 00:10:21 +0100

    update

---
+++ assets/script/area_lang.js
@@ -0,0 +1,43 @@
+var filename = document.getElementById('filename').innerHTML
+var lang = localStorage.getItem('language')
+var xmlhttp = new XMLHttpRequest();
+var url ="../assets/lang/"+lang+"/"+filename+".json"
+var common ="../assets/lang/"+lang+"/area_common.json"
+
+window.onload = check;
+function check(){
+if (lang === null){
+	window.location.href = "lang.html";
+	setTimeout(check, 300);
+}else{
+	setTimeout(pop, 10);
+	setTimeout(comm, 10);
+};
+function comm(){
+xmlhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    var com = JSON.parse(this.responseText);
+	document.getElementById("colpick").firstElementChild.innerHTML=com.colpick;
+	document.getElementById("csquare").innerHTML=com.csquare;
+	document.getElementById("notes").innerHTML=com.notes;
+	document.getElementById("textArea").innerHTML=com.textArea;
+	document.getElementById("save").innerHTML=com.save;
+	document.getElementById("wipe").innerHTML=com.wipe;
+  }
+};
+xmlhttp.open("GET", common, true);
+xmlhttp.send();
+}
+function pop(){
+xmlhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    var file = JSON.parse(this.responseText);
+    console.log(file)
+    alert(file.lang)
+    alert(file.text)
+  }
+};
+xmlhttp.open("GET", url, true);
+xmlhttp.send();
+}
+}
--- assets/script/lang.js
+++ assets/script/lang.js
@@ -2,7 +2,6 @@ var filename = document.getElementById('filename').innerHTML
 var lang = localStorage.getItem('language')
 var xmlhttp = new XMLHttpRequest();
 var url ="assets/lang/"+lang+"/"+filename+".json"
-var common ="assets/lang/"+lang+"/area_common.json"
 
 window.onload = check;
 function check(){
@@ -13,25 +12,6 @@ if (lang === null){
 	setTimeout(pop, 10);
 	setTimeout(comm, 10);
 };
-function comm(){
-xmlhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-    var com = JSON.parse(this.responseText);
-    console.log(file)
-    alert(file.lang)
-    alert(file.text)
-	
-	document.getElementById("colpick").firstElementChild.innerHTML=com.colpick;
-	document.getElementById("csquare").innerHTML=com.csquare;
-	document.getElementById("notes").innerHTML=com.notes;
-	document.getElementById("textArea").innerHTML=com.textArea;
-	document.getElementById("save").innerHTML=com.save;
-	document.getElementById("wipe").innerHTML=com.wipe;
-  }
-};
-xmlhttp.open("GET", common, true);
-xmlhttp.send();
-}
 function pop(){
 xmlhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
