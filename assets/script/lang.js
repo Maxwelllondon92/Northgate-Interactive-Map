2c6ed3031ef65833b0906199d96d21e75125baa8
Maxwelllondon92 <Maxwelllondon92@gmail.com>
2020-10-28 21:21:46 +0100

    Update lang.js

--- assets/script/lang.js
+++ assets/script/lang.js
@@ -1,9 +1,13 @@
+
+
 window.onload = function(){
 var filename = document.getElementById('filename').innerHTML
 var lang = localStorage.getItem('language')
 var xmlhttp = new XMLHttpRequest();
 var url ="assets/lang/"+lang+"/"+filename+".json"
-
+if lang=null{
+	localStorage.setItem('localstorage','en')
+};
 xmlhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
     var file = JSON.parse(this.responseText);
